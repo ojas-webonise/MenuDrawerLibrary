fd05007c97790b79a9fb654adfa1eea50a0097ea
Ojas <ojas@weboniselab.com>
2013-12-16 11:44:29 +0530

    first commit

---
+++ src/com/menudrawerlibrary/DrawerActivity.java
@@ -0,0 +1,12 @@
+package com.menudrawerlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawerActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ src/com/menudrawerlibrary/SmoothInterpolator.java
@@ -0,0 +1,12 @@
+package com.menudrawerlibrary;
+
+import android.view.animation.Interpolator;
+
+public class SmoothInterpolator implements Interpolator {
+
+    @Override
+    public float getInterpolation(float t) {
+        t -= 1.0f;
+        return t * t * t * t * t + 1.0f;
+    }
+}
